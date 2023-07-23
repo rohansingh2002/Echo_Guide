8943ebe578d540c99960cc247899088826bdcc3b
rohansingh2002 <121345451+rohansingh2002@users.noreply.github.com>
2023-07-23 21:48:10 +0530

    Update and rename Send_whatsapp_message. to Send_whatsapp_message.py

--- features/Send_whatsapp_message.py
+++ features/Send_whatsapp_message.py
@@ -1,4 +1,8 @@
-if 'send a message' in command:                     # This statement is send message
+def run_allenite():
+    command = take_command()
+    print(command)
+   
+    if 'send a message' in command:                     # This statement is send message
         with sr.Microphone() as source:
             talk("what's the number")                   
             voice = listener.listen(source)             #This statement is used listen the voice
@@ -11,3 +15,8 @@ if 'send a message' in command:                     # This statement is send mes
             print(msg)
         pywhatkit.sendwhatmsg('+91'+no, msg,datetime.datetime.now().hour,datetime.datetime.now().minute+1)        # This statement is used to send whatsapp message
       
+
+talk('Heyy,I am Allenite, How may i Help You')
+
+while True:
+    run_allenite()                     
