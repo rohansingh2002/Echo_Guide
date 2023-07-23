a73698ac1a7465a65caa5d745708368379699f78
rohansingh2002 <121345451+rohansingh2002@users.noreply.github.com>
2023-07-23 21:47:27 +0530

    Update wikipedia_search_feature.py

--- features/wikipedia_search_feature.py
+++ features/wikipedia_search_feature.py
@@ -1,3 +1,15 @@
- elif 'open' in command:                               # This statement is used to open any link
+def run_allenite():
+    command = take_command()
+    print(command)
+   
+    if 'open' in command:                               # This statement is used to open any link
         link = command.replace('open ','')
         webbrowser.open(link)
+
+    
+  
+
+talk('Heyy,I am Allenite, How may i Help You')
+
+while True:
+    run_allenite()                     
